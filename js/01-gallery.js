a14f6d5c508de411ceaa10d2633480ef9ec09c68
YuriyBeregovoy <rik0681@gmail.com>
2023-06-10 20:08:43 +0300

    created the markup according to the template

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+console.log(createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `;
+  }).join("");
+
+};
+
+
+
 console.log(galleryItems);
