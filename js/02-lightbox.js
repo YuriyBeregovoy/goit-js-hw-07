068970d0ecbf8538663138a384c16eb103cfba1d
YuriyBeregovoy <rik0681@gmail.com>
2023-06-11 16:39:26 +0300

    added the markup of the part 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup)
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;}).join("");
+};
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+
+  const swatchEl = evt.target;
+  
+  if (!swatchEl.classList.contains("gallery__image")) {
+    return;
+  }
+
+  // const gallaryLargeImgSource = swatchEl.dataset.source
+   
+  // addModalWindowGallery(gallaryLargeImgSource);
+  
+};
+
+
+// let instance;
+
+// function addModalWindowGallery(url) {
+//   window.addEventListener("keydown", onEscKeyPress);
+//   instance = basicLightbox.create(`
+//     <img src="${url}" width="800" height="600">
+//   `);
+//   instance.show();
+// };
+
+
+// function onEscKeyPress(evt) {
+ 
+//   if (evt.code === "Escape") {
+//      window.removeEventListener("keydown", onEscKeyPress); 
+//     instance.close();
+//   };
+//   console.log(evt);
+// };
+
+
+
