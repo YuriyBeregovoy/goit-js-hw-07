5ed659ee5f3348d935238ff2faf70ec5a0995c2d
YuriyBeregovoy <rik0681@gmail.com>
2023-06-11 14:29:09 +0300

    added closing the modal window with ESC

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -37,13 +37,24 @@ function onGalleryContainerClick(evt) {
   
 };
 
+let instance;
+
 function addModalWindowGallery(url) {
-  const instance = basicLightbox.create(`
+  window.addEventListener("keydown", onEscKeyPress);
+  instance = basicLightbox.create(`
     <img src="${url}" width="800" height="600">
   `);
   instance.show();
+    }
+
+
+function onEscKeyPress(evt) {
+ 
+  if (evt.code === "Escape") {
+     window.removeEventListener("keydown", onEscKeyPress); 
+    instance.close();
+  }
+  console.log(evt);
 }
-
-
 
 
